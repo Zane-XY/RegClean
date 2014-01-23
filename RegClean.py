8a153d6c9375010e7024ab9580fe8ee06afd5b9b
Zane.XY <zane.xy@live.com>
2014-01-23 15:47:05 +0800

    fixed sid can't be get dynamically

--- RegClean.py
+++ RegClean.py
@@ -1,9 +1,13 @@
+import os
 from winreg import *
 
-app_names = ["spotify", "evernote", "VLC"]
+appNames = ["spotify", "evernote", "VLC"]
 
 
-def detect(text, names=app_names):
+username = os.getenv("USERNAME")
+
+
+def detect(text, names=appNames):
     for name in names:
         if text.lower().find(name.lower()) > -1:
             print("found in name list : " + text)
@@ -11,54 +15,82 @@ def detect(text, names=app_names):
     return False
 
 
-def remove_from_uninstall(key, subkey):
+def removeFromUninstall(key, subkey):
     print("deleted : " + subkey)
     DeleteKeyEx(key, subkey)
 
 
-def clean_key(key):
-    sub_key_counts = QueryInfoKey(key)[0]
-    for i in range(sub_key_counts):
+# takes a predicate function op
+def loopKeys(key, op):
+    subKeyCounts = QueryInfoKey(key)[0]
+    for i in range(subKeyCounts):
         try:
-            subkey_name = EnumKey(key, i)
-            subkey = OpenKey(key, subkey_name)
-            display_name = (QueryValueEx(subkey, "DisplayName")[0])
-            if "Microsoft" not in display_name:
-                print("found app : ", display_name)
-                if detect(display_name):
-                    remove_from_uninstall(key, subkey_name)
-        except FileNotFoundError:
-            print("'DisplayName' is not found in " + subkey_name)
+            subkeyName = EnumKey(key, i)
+            subkey = OpenKey(key, subkeyName)
+            if op(subkey, subkeyName):
+                return (subkey, subkeyName)
+            #print(subkey)
+        #except FileNotFoundError:
+            #print("'DisplayName' is not found in " + subkeyName)
         except UnicodeEncodeError:
-            print("UnicodeEncodeError for key " + subkey_name)
+            print("UnicodeEncodeError for key " + subkeyName)
         except OSError:
-            print("already deleted: " + subkey_name)
+            print("already deleted: " + subkeyName)
 
 
-def user_reg():
+def userReg():
     #SID is in HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList
-    reg = ConnectRegistry(None, HKEY_USERS)
-    return OpenKey(reg, r"S-1-5-21-2380125314-1135609519-1071393968-13754\Software\Microsoft\Windows\CurrentVersion\Uninstall")
+    reg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+    profileKey = OpenKey(reg, r"SOFTWARE\Microsoft\Windows NT\CurrentVersion\ProfileList")
+    sidKey = loopKeys(profileKey, lambda k, n: filterSID(k, n))
+    userKey = ConnectRegistry(None, HKEY_USERS)
+    return OpenKey(userKey, str(sidKey[1]) + r"\Software\Microsoft\Windows\CurrentVersion\Uninstall")
 
 
-def sys_reg():
+def sysReg():
     reg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
     return OpenKey(reg, r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall")
 
 
-def sys64_reg():
+def sys64Reg():
     reg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
     return OpenKey(reg, r"SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall")
 
 
-def clean():
-    #clean_key(user_reg())
-    clean_key(sys_reg())
-    clean_key(sys64_reg())
+def clean(reg):
+    loopKeys(reg, lambda x, y: cleanKey(x, y))
+
+
+# sid predicate
+def filterSID(subkey, subkeyName):
+    try:
+        profilePath = (QueryValueEx(subkey, "ProfileImagePath")[0])
+        if username in profilePath:
+            return True
+        else:
+            return False
+    except FileNotFoundError:
+        #print("'ProfileImagePath' is not found in " + subkeyName)
+        pass
+
+
+def cleanKey(subkey, subkeyName):
+    try:
+        displayName = (QueryValueEx(subkey, "DisplayName")[0])
+        if "Microsoft" not in displayName and "Windows Driver" not in displayName:
+            print("found app : ", displayName)
+            if detect(displayName):
+                removeFromUninstall(key, subkeyName)
+        return False
+    except FileNotFoundError:
+        #print("'DisplayName' is not found in " + subkeyName)
+        pass
 
 
 def main():
-    clean()
+    clean(userReg())
+    clean(sysReg())
+    clean(sys64Reg())
 
 
 if __name__ == "__main__":
